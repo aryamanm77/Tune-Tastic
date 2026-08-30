d9f77a1f762d62f01d2478c3bbe05632c012e11d
aryamanm77 <aryamanm77@gmail.com>
2026-08-30 19:18:27 +0530

    chore: Move Firebase credentials to .env to prevent GitHub secret alerts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDr9DpDGio2TMPpEa0iEY2tUF6IMfqnRKE",
-  authDomain: "tunetastic-d8ecc.firebaseapp.com",
-  projectId: "tunetastic-d8ecc",
-  storageBucket: "tunetastic-d8ecc.firebasestorage.app",
-  messagingSenderId: "817944483782",
-  appId: "1:817944483782:web:02141bce3cc823efe4c4c7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
